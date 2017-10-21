ee301fdf028cfc0f24d8b52b86e6d1a27638f91f
Jacob Denholtz <jdenholtz@email.arizona.edu>
2017-10-21 22:00:56 +0000

    new gram page working

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -3,4 +3,7 @@ class GramsController < ApplicationController
   def index
   end
   
+  def new
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "grams#index"
+
+  resource :grams
 end
